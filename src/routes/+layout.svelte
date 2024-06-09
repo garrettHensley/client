9d4565a5441ac2f2906f949ecd09feead91491b0
Garrett Hensley <ghensley@fwcjua.com>
2024-06-09 10:49:04 -0400

    shit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
     import "../app.css";
+    import { jwtDecode } from 'jwt-decode';
     import { Styles } from '@sveltestrap/sveltestrap';
+    import { onMount } from 'svelte';
     import {
     Collapse,
     NavbarToggler,
@@ -18,10 +20,35 @@
   } from '@sveltestrap/sveltestrap';
 
   let isOpen = false;
+  let loggedIn = false;
+  let username = '';
+  
+  const decodeToken = () => {
+    const cookies = document.cookie.split(';');
+let jwtToken = '';
+console.log(document.cookie)
+for (const cookie of cookies) {
+    const [name, value] = cookie.trim().split('=');
+    console.log(name)
+    if (name === 'jwtToken') {
+        jwtToken = decodeURIComponent(value);
+        break;
+    }
+}
+  if (jwtToken) {
+    // Decode the JWT token to extract user information
+    const decodedToken = jwtDecode(jwtToken);
+    username = decodedToken.username; 
+  } else {
+    console.log('No JWT token found')
+  }
+  }
 
   function handleUpdate(event) {
     isOpen = event.detail.isOpen;
   }
+
+  onMount(decodeToken);
   </script>
   
   <svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-bitch
+<script>
+    let loginUrl = "https://discord.com/oauth2/authorize?client_id=1248372466682691717&response_type=code&redirect_uri=https%3A%2F%2Flocalhost%3A7108%2Fauth%2Fdiscord-callback&scope=identify"
+</script>
+
+<main>
+    <h1>Login with Discord</h1>
+    <a href={loginUrl}>Login with Discord</a>
+</main>
