2cb6db881648ec2428b71db0020e0b0171db5a94
Garrett Hensley <ghensley@fwcjua.com>
2024-06-19 14:37:25 -0400

    this fuckn bullshit works now

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -6,26 +6,13 @@ export async function load({ cookies}) {
 	const isLoggedIn = false;
 	const loginUrl = import.meta.env.VITE_REDIRECT_URI
 	const testUrl = `${import.meta.env.VITE_AUTH_URL}/TestMe`
-	const authUrl = `${import.meta.env.VITE_AUTH_URL}/auth/discord-callback`
-	// console.log('token', cookies.getAll())
-	// if (token) {
-	// 	// const { unique_name, userId } = jwtDecode(token);
-
-	// 	return {
-	// 		jwtToken: token,
-	// 		isLoggedIn: true,
-	// 		userName: unique_name,
-	// 		userId,
-	// 		loginUrl,
-	// 		testUrl,
-
-	// 	};
-	// }
+	const authUrl = `${import.meta.env.VITE_AUTH_URL}/discord-callback`
 
 	return {
 		isLoggedIn: false,
 		loginUrl,
 		// jwtToken: token,
+		token,
 		testUrl,
 		authUrl,
 	};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,7 +28,7 @@
   let userId = '';
   const loginUrl = data.loginUrl;
   
-  const code = $page.url.searchParams.get('code')
+  let code = $page.url.searchParams.get('code')
   console.log(data)
   
   if (data.isLoggedIn) {
@@ -45,26 +45,74 @@
     isOpen = event.detail.isOpen;
   }
 
-  const authme = async () => {
-    console.log('autthing')
-    if (code) {
-      console.log('has code')
+  const testAuth = async () => {
+    const something = await fetch('https://localhost:32778/auth/TestMe', {
+              method: 'GET',
+              credentials: 'include',
+            })
 
-      const response = await fetch(`${import.meta.env.VITE_AUTH_URL}/discord-callback?code=${code}`);
-      const dat = await response.json();
-
-    }
-    else {
-      const reponse = await fetch(`${data.testUrl}`, {
-        method: 'GET',
-        credentials: 'include',
-      })
-      const dat = await response.json();
-      console.log(dat)
-    }
+            const res = await something.json()
+            console.log(res)
   }
 
-  onMount(authme);
+  const authme = async () => {
+    console.log('autthing')
+  //   if (code) {
+  //     console.log('has code')
+
+  //     const response = await fetch(`${import.meta.env.VITE_AUTH_URL}/discord-callback`, {
+  //       method: 'POST',
+  //       headers: {
+  //   'Content-Type': 'text/plain', // Set content type as plain text
+  // },
+  // body: code
+  //     });
+
+      if (code) {
+        const url = 'https://localhost:32778/auth/discord-callback';
+        // window.location.href = url + '?code=' + code;
+        const someting = await fetch(url+ '?code=' + code)
+        const dat = await someting.json()
+        console.log(dat)
+        // const codeValue = code; // Replace with your actual string value
+
+        // fetch(url, {
+        //     method: 'POST',
+        //     headers: {
+        //       'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({ code: codeValue })
+        //   })
+        //   .then(response => {
+        //     if (!response.ok) {
+        //       throw new Error('Network response was not ok');
+        //     }
+        //     return response.json();
+        //   })
+        //   .then(data => {
+        //     console.log('Success:', data);
+        //   })
+        //   .catch(error => {
+        //     console.error('Error:', error);
+        //   });
+      }
+      else {
+
+      }
+
+
+    }
+    // else {
+    //   const reponse = await fetch(`${data.testUrl}`, {
+    //     method: 'GET',
+    //     credentials: 'include',
+    //   })
+    //   const dat = await response.json();
+    //   console.log(dat)
+    // }
+  
+
+  // onMount(authme);
 
   // onMount(decodeToken);
 
@@ -77,7 +125,7 @@
   <a href="{data.testUrl}">test button test me</a>
 <Navbar color="light" light expand="md" container="md">
   <NavbarBrand href="/">
-    <button on:click={testThing}>welcome</button>
+    <button on:click={testAuth}>welcome</button>
       <Button outline=true color=primary href="/build">
         <Icon name="plus-circle-dotted" />
         Build
@@ -99,7 +147,7 @@
       
         {:else}
           <NavItem>
-            <NavLink href="{loginUrl}">Sign In</NavLink>
+            <NavLink href="https://discord.com/oauth2/authorize?client_id=1248372466682691717&response_type=code&redirect_uri=https%3A%2F%2Flocalhost%3A32778%2Fauth%2Fdiscord-callback&scope=identify">Sign In</NavLink>
           </NavItem>
       {/if}
       
